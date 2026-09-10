c27ada1632d8d7abba2c1d5ff7e7cf819d7cab8a
jyoon-bio <jyoon0645@gmail.com>
2026-09-11 01:45:48 +0900

    Bump archive cache version for post 009

--- archive-cms.js
+++ archive-cms.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const API_VERSION = '20260902-content-types-v3';
+  const API_VERSION = '20260911-post009-v4';
   const LIST_API_URL = `/api/archive?v=${API_VERSION}`;
   const DETAIL_API_URL = '/api/archive-detail';
   const CACHE_KEY = 'young-bio-archive-list-v3';
