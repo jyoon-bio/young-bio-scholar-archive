8453926dc5813bb4c643e250ec79a798c2c7534d
jyoon-bio <jyoon0645@gmail.com>
2026-09-19 20:13:25 +0900

    Publish Post 16 on antibiotic resistance

--- archive-cms.js
+++ archive-cms.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const API_VERSION = '20260919-post013-v1';
+  const API_VERSION = '20260919-post014-v1';
   const LIST_API_URL = `/api/archive?v=${API_VERSION}`;
   const DETAIL_API_URL = '/api/archive-detail';
   const CACHE_KEY = 'young-bio-archive-list-v4';
